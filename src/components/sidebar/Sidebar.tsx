468c99190cd0ecf7a7a6897ab37e6003ddcfa5b0
hefx <1074535830@qq.com>
2022-11-09 17:03:49 +0800

    add sidebar.scss

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Sidebar() {
-  return <div>this is sidebar</div>;
+  return (
+    <div sidebar-warpper>
+      <div></div>
+      this is sidebar
+    </div>
+  );
 }
 
 export default Sidebar;
