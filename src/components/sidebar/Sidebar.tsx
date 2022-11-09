7aaa0d77a7865c9c9b934264bdab4634f926ee08
hefx <1074535830@qq.com>
2022-11-09 18:55:16 +0800

    new userinfo

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,38 +5,51 @@ function Sidebar() {
   return (
     <div className="sidebar-warpper">
       <div className="items-wrapper">
-        <div className="item">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-gailan"></use>
-          </svg>
-          <span>概览</span>
+        <div className="item-groups">
+          <div className="first-item">
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-gailan"></use>
+            </svg>
+            <span>概览</span>
+          </div>
+          <div className="divider"></div>
+
+          <div className="second-item">
+            <span>个人中心</span>
+          </div>
         </div>
 
         <div className="divider"></div>
 
-        <div className="item">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-zujian"></use>
-          </svg>
-          <span>组件</span>
+        <div className="item-groups">
+          <div className="first-item">
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-zujian"></use>
+            </svg>
+            <span>组件</span>
+          </div>
         </div>
 
         <div className="divider"></div>
 
-        <div className="item">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-wenjianjia"></use>
-          </svg>
-          <span>文档</span>
+        <div className="item-groups">
+          <div className="first-item">
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-wenjianjia"></use>
+            </svg>
+            <span>文档</span>
+          </div>
         </div>
 
         <div className="divider"></div>
 
-        <div className="item">
-          <svg className="icon" aria-hidden="true">
-            <use xlinkHref="#icon-shezhi"></use>
-          </svg>
-          <span>设置</span>
+        <div className="item-groups">
+          <div className="first-item">
+            <svg className="icon" aria-hidden="true">
+              <use xlinkHref="#icon-shezhi"></use>
+            </svg>
+            <span>设置</span>
+          </div>
         </div>
       </div>
     </div>
