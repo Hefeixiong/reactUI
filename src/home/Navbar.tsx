2b320e8501168632a5541e65e00bc9425acc9fad
hefx <1074535830@qq.com>
2022-11-09 14:25:47 +0800

    navbar--menu

--- src/home/Navbar.tsx
+++ src/home/Navbar.tsx
@@ -3,21 +3,50 @@ import "./styles/navbar.scss";
 
 const Navbar = () => {
   return (
-    <ol className="navbar">
-      <li className="logo-name">
-        <svg className="icon" aria-hidden="true">
-          <use xlinkHref="#icon-main"></use>
-        </svg>
-        <div className="name">ReactUI</div>
-      </li>
+    <div className="navbar-wrapper">
+      <ol className="navbar">
+        <li className="logo-name">
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-main"></use>
+          </svg>
+          <div className="name">ReactUI</div>
+        </li>
 
-      <li className="middle">我的官网</li>
+        <li className="middle">我的官网</li>
 
-      <li className="user-info">
-        <div className="avatar">xxx</div>
-        <div>username</div>
-      </li>
-    </ol>
+        <li className="user-info">
+          <div className="avatar">xxx</div>
+          <div className="username">
+            <span>hefx</span>
+            <svg className="icon-dowm" aria-hidden="true">
+              <use xlinkHref="#icon-down"></use>
+            </svg>
+          </div>
+          <ol className="menu-wrapper">
+            <div className="items">
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-user-fill"></use>
+              </svg>
+              <span>用户中心</span>
+            </div>
+            <div className="divider"></div>
+            <div className="items">
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-yuyan"></use>
+              </svg>
+              <span> 切换语言 </span>
+            </div>
+            <div className="divider"></div>
+            <div className="items">
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-tuichuzhuxiao"></use>
+              </svg>
+              <span> 注销 </span>
+            </div>
+          </ol>
+        </li>
+      </ol>
+    </div>
   );
 };
 
