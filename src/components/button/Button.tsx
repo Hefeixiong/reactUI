cb692291a837bd383a6a395f23f8e57f06d7864c
hefx <1074535830@qq.com>
2022-11-16 22:12:19 +0800

    新增button组件

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { FC } from "react";
+import "./button.scss";
 
-const Button = () => {
-  return <div>button</div>;
+type buttonProp = {
+  text: string;
+  type: string;
+  onClick?: (value: string) => void;
+};
+
+const Button: FC<buttonProp> = (props) => {
+  return (
+    <button className={"button " + props.type}>
+      <span>{props.text}</span>
+    </button>
+  );
 };
 
 export default Button;
--- src/views/modules/index.tsx
+++ src/views/modules/index.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { rainbow } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import Button from "../../components/button/Button";
 
 function CodeView() {
-  const codeString = `function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="doc" element={<Doc />}>
-            <Route path="userinfo" element={<UserInfo />} />
-            <Route path="notes" element={<Notes />} />
-            <Route path="codeview" element={<CodeView />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }`;
   return (
-    <SyntaxHighlighter language="javascript" style={rainbow}>
-      {codeString}
-    </SyntaxHighlighter>
+    <div className="view-wrapper">
+      <div>
+        <Button text={"xxx"} type="warn" />
+        <Button text={"xxx"} type="default" />
+        <Button text={"xxx"} type="primary" />
+      </div>
+      <button>隐藏/显示</button>
+      <div>
+        <SyntaxHighlighter language="javascript" style={rainbow}>
+          {`<Button text={"xxx"} type="warn" />`}
+        </SyntaxHighlighter>
+      </div>
+    </div>
   );
 }
 
