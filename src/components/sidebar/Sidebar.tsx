3ab4f5bc1ed98f2e9bb0fe5c34617b8818355055
hefx <1074535830@qq.com>
2022-11-19 16:21:11 +0800

    调整整体样式+重构sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,51 +4,51 @@ import Divider from "../divider";
 import "./sidebar.scss";
 
 function Sidebar() {
+  const xxx = [
+    {
+      id: 0,
+      title: "个人中心",
+      icon: "#icon-gailan",
+      url: "/doc/userinfo",
+      children: {},
+    },
+    {
+      id: 1,
+      title: "组件",
+      icon: "#icon-zujian",
+      url: "/doc/buttonDemo",
+      children: {},
+    },
+    {
+      id: 2,
+      title: "文档",
+      icon: "#icon-wenjianjia",
+      url: "/doc/notes",
+      children: {},
+    },
+    {
+      id: 3,
+      title: "设置",
+      icon: "#icon-shezhi",
+      url: "/doc/buttonDemo",
+      children: {},
+    },
+  ];
   return (
-    <div className="sidebar-warpper">
-      <div className="items-wrapper">
-        <div className="item-groups">
-          <Link className="first-item" to="/doc/userinfo">
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-gailan"></use>
-            </svg>
-            <span>个人中心</span>
-          </Link>
-        </div>
-
-        <Divider />
-
-        <div className="item-groups">
-          <Link className="first-item" to="/doc/buttonDemo">
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-zujian"></use>
-            </svg>
-            <span>组件</span>
-          </Link>
-        </div>
-
-        <Divider />
-
-        <div className="item-groups">
-          <Link className="first-item" to={"/doc/notes"}>
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-wenjianjia"></use>
-            </svg>
-            <span>文档</span>
-          </Link>
-        </div>
-
-        <Divider />
-
-        <div className="item-groups">
-          <div className="first-item">
-            <svg className="icon" aria-hidden="true">
-              <use xlinkHref="#icon-shezhi"></use>
-            </svg>
-            <span>设置</span>
+    <div className="items-wrapper">
+      {xxx.map((item) => {
+        return (
+          <div className="item-groups" key={item.id}>
+            <Link className="first-item" to={item.url}>
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref={item.icon}></use>
+              </svg>
+              <span>{item.title}</span>
+            </Link>
+            <Divider />
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
